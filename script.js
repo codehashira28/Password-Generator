47b9cf15126dc5eb32d300c98fdb04981b0c5347
codehashira28 <123044813+codehashira28@users.noreply.github.com>
2023-04-28 14:44:26 -0500

    added logic for first phase of project to ask for password length

--- script.js
+++ script.js
@@ -1,10 +1,21 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-function generatePassword() {
-  var lowercase = "abcdefghijklmnopqrstuvwxyz";
 
-  var result = prompt();
+
+function generatePassword() {
+  // Declaration of acceptable character variables
+
+  var lowercase = "abcdefghijklmnopqrstuvwxyz";
+  var uppercase = lowercase.toUpperCase();
+  var specialChars = "!#$%&'()*+,-.:;<=>?@[]^_`{|}~";
+
+  // logic for initial prompt asking for number of characters for password
+
+  var numChars = prompt('Please enter a password length between 8 and 128 characters.');
+  while(numChars < 8 || numChars > 128) {
+    numChars = prompt('Please enter a password length between 8 and 128 characters.');
+  }
 
   var random = Math.floor(Math.random() * lowercase.length);
 
