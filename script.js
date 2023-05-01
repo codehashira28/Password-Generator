8bb026072af7f73470c7f3f95e09f98e8c907814
codehashira28 <123044813+codehashira28@users.noreply.github.com>
2023-05-01 11:40:02 -0500

    added rest of the logic to generate password based on selected criteria. Also considered edge cases if user inputs unexpected password length

--- script.js
+++ script.js
@@ -9,23 +9,69 @@ function generatePassword() {
   var lowercase = "abcdefghijklmnopqrstuvwxyz";
   var uppercase = lowercase.toUpperCase();
   var specialChars = "!#$%&'()*+,-.:;<=>?@[]^_`{|}~";
+  var numbers = "0123456789";
 
   // logic for initial prompt asking for number of characters for password
 
-  var numChars = prompt('Please enter a password length between 8 and 128 characters.');
+  var numChars = prompt('How many characters do you want your password to be? (Must be between 8-128 characters long)');
 
-  while((numChars < 8 || numChars > 128) && numChars != null) {
-    numChars = prompt('Please enter a password length between 8 and 128 characters.');
+  // conditions to handle unexpected inputs (strings, decimals, and numbers outside the range)
+
+  while(((numChars < 8 || numChars > 128) && numChars != null) || (Math.floor(numChars) != numChars && numChars != null) || (isNaN(parseInt(numChars))) && numChars != null) { 
+    numChars = prompt('Please enter a whole number length between 8 and 128 characters.');
   }
+
+  // if no valid length is entered and prompt is cancelled, return an empty string,
+  // otherwise generate password based on selected credentials
 
   if(numChars === null) {
     return "";
-  }
+  } else {
 
-  var random = Math.floor(Math.random() * lowercase.length);
+    var wantLowercase = confirm("Include lowercase letters?");
+    var wantUppercase = confirm("Include uppercase letters?");
+    var wantNumerics = confirm("Include numbers?");
+    var wantSpecialChars = confirm("Include special characters?");
 
+    var password = "";
   
+    while(password.length < numChars) {
+      if(wantLowercase) {
+        if(password.length == numChars) {
+          break;
+        }
+        password += lowercase[Math.floor(Math.random() * lowercase.length)];
+      }
+
+      if(wantUppercase) {
+        if(password.length == numChars) {
+          break;
+        }
+        password += uppercase[Math.floor(Math.random() * uppercase.length)];
+      }
+
+      if(wantNumerics) {
+        if(password.length == numChars) {
+          break;
+        }
+        password += numbers[Math.floor(Math.random() * numbers.length)];
+      }
+
+      if(wantSpecialChars) {
+        if(password.length == numChars) {
+          break;
+        }
+        password += specialChars[Math.floor(Math.random() * specialChars.length)];
+      }
+
+      if(!wantLowercase && !wantUppercase && !wantSpecialChars && !wantNumerics) {
+        break;
+      }
+    }
+      return password;
+  }
 }
+
 
 // Write password to the #password input
 function writePassword() {
